d645d65170f4f19a563d1c79c2d966246e9f40f8
mide42 <demi_05@arcor.de>
2019-05-25 17:33:13 +0200

    Upgrade auf Android Studio 3.4

--- app/src/main/java/de/mide/webview_demo/MainActivity.java
+++ app/src/main/java/de/mide/webview_demo/MainActivity.java
@@ -56,6 +56,7 @@ public class MainActivity extends Activity implements OnClickListener {
 	 */	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
+
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 		
@@ -192,6 +193,7 @@ public class MainActivity extends Activity implements OnClickListener {
 	 * HTML-Datei muss mit Kodierung <i>"UTF-8"</i> gespeichert sein.
 	 */
 	protected void ladeHilfeSeite() {
+
 		try {
 			
 			InputStream    is     = getResources().openRawResource(R.raw.hilfe);
